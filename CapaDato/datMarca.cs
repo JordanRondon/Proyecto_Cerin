d41b06a885ffab0b5915223b652e85e728be560c
JordanRondon <jordanangelrondonpozo@gmail.com>
2023-08-26 09:24:26 -0500

    se agrego el metodo listarMarcas a la capa de datos

---
+++ CapaDato/datMarca.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+
+namespace CapaDato
+{
+    public class datMarca
+    {
+        #region Singleton
+        private static readonly datMarca instancia = new datMarca();
+        public static datMarca GetInstancia => instancia;
+        #endregion
+
+        #region Metodos
+        public List<entMarca> listarMarcas()
+        {
+            SqlCommand cmd = null;
+            List<entMarca> lista = new List<entMarca>();
+
+            try
+            {
+                SqlConnection cn = Conexion.GetInstancia.Conectar; //singleton
+                
+                cmd = new SqlCommand("sp_listarMarca", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cn.Open();
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    entMarca marca = new entMarca();
+
+                    marca.IdMarca = Convert.ToInt32(dr["id_Marca"]);
+                    marca.Nombre = Convert.ToString(dr["nombre"]);
+                    
+                    lista.Add(marca);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                cmd.Connection.Close();
+            }
+
+            return lista;
+        }
+        #endregion
+    }
+}
