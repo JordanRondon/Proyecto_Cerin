9360983275fd4b83be2eddec4335f483e5d903f6
JordanRondon <jordanangelrondonpozo@gmail.com>
2023-08-26 18:33:35 -0500

    se agrego los metodos de ingresar, modificar y eliminar en la capa datos. Como tambien se agrego los metodos de ellos en la capa logica

--- CapaDato/datEmpleado.cs
+++ CapaDato/datEmpleado.cs
@@ -55,6 +55,109 @@ namespace CapaDato
 
             return lista;
         }
+
+        public bool insertarEmpleado(entEmpleado empleado)
+        {
+            SqlCommand cmd = null;
+            bool inserta = false;
+
+            try
+            {
+                SqlConnection cn = Conexion.GetInstancia.Conectar; //singleton
+
+                cmd = new SqlCommand("sp_insertarEmpleado", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@nombre", empleado.Nombre);
+                cmd.Parameters.AddWithValue("@apellido", empleado.Apellido);
+                cmd.Parameters.AddWithValue("@dni", empleado.Dni);
+                cmd.Parameters.AddWithValue("@direccion", empleado.Direccion);
+                cmd.Parameters.AddWithValue("@correo", empleado.Correo);
+                cmd.Parameters.AddWithValue("@telefono", empleado.Telefono);
+
+                cn.Open();
+
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    inserta = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally { cmd.Connection.Close(); }
+
+            return inserta;
+        }
+
+        public bool editarEmpleado(entEmpleado empleado)
+        {
+            SqlCommand cmd = null;
+            bool edita = false;
+
+            try
+            {
+                SqlConnection cn = Conexion.GetInstancia.Conectar;
+
+                cmd = new SqlCommand("sp_modificarEmpleado", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@id_empleado", empleado.IdEmpleado);
+                cmd.Parameters.AddWithValue("@nombre", empleado.Nombre);
+                cmd.Parameters.AddWithValue("@apellido", empleado.Apellido);
+                cmd.Parameters.AddWithValue("@dni", empleado.Dni);
+                cmd.Parameters.AddWithValue("@direccion", empleado.Direccion);
+                cmd.Parameters.AddWithValue("@correo", empleado.Correo);
+                cmd.Parameters.AddWithValue("@telefono", empleado.Telefono);
+
+                cn.Open();
+
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    edita = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally { cmd.Connection.Close(); }
+
+            return edita;
+        }
+
+        public bool eliminarEmpleado(entEmpleado empleado)
+        {
+            SqlCommand cmd = null;
+            bool seElimino = false;
+
+            try
+            {
+                SqlConnection cn = Conexion.GetInstancia.Conectar;
+                cmd = new SqlCommand("sp_eliminarEmpleado", cn);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@@id_empleado", empleado.IdEmpleado);
+
+                cn.Open();
+
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    seElimino = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally { cmd.Connection.Close(); }
+
+            return seElimino;
+        }
         #endregion
     }
 }
---
+++ CapaLogica/logEmpleado.cs
@@ -0,0 +1,28 @@
+﻿using CapaDato;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class logEmpleado
+    {
+        #region Singleton
+        private static readonly logEmpleado instancia = new logEmpleado();
+        public static logEmpleado GetInstancia => instancia;
+        #endregion
+
+        #region Metodos
+        public List<entEmpleado> listarProducto() => datEmpleado.GetInstancia.listarEmpleado();
+
+        public bool insertaEmpleado(entEmpleado empleado) => datEmpleado.GetInstancia.insertarEmpleado(empleado);
+
+        public bool editarEmpleado(entEmpleado empleado) => datEmpleado.GetInstancia.editarEmpleado(empleado);
+
+        public bool eliminarEmpleado(entEmpleado empleado) => datEmpleado.GetInstancia.eliminarEmpleado(empleado);
+        #endregion
+    }
+}
