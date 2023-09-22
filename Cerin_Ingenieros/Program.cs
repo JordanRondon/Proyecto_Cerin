70fd27ce8bdefc964c56568760585bc90fa3076b
JordanRondon <jordanangelrondonpozo@gmail.com>
2023-09-21 19:39:22 -0500

    cambio del metodo rum a el formulario Principal

--- Cerin_Ingenieros/Program.cs
+++ Cerin_Ingenieros/Program.cs
@@ -21,7 +21,7 @@ namespace Cerin_Ingenieros
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new preHistorialEquipo());
+            Application.Run(new Principal());
         }
     }
 }
