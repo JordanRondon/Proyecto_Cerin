20dfda3559df0c006ca5cf8ecb96be5ff399104e
JordanRondon <jordanangelrondonpozo@gmail.com>
2023-09-30 09:43:17 -0500

    maximizado y minimizado desde el ico de la barra de escritorio

--- Cerin_Ingenieros/Principal/Principal.cs
+++ Cerin_Ingenieros/Principal/Principal.cs
@@ -17,6 +17,21 @@ namespace Cerin_Ingenieros
 {
     public partial class Principal : Form
     {
+        //permite minimizar y maximizar desde el icono del la barra de tareas
+        const int WS_MINIMIZEBOX = 0x20000;
+        const int CS_DBLCLKS = 0x8;
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.Style |= WS_MINIMIZEBOX;
+                cp.ClassStyle |= CS_DBLCLKS;
+                return cp;
+            }
+        }
+        //---------------------------------------------------------------------
         //FUNCION PARA MOVIMIENTO DEL FORMULARIO
         private bool isMinimized = false;
         private int m, mx, my;
